7da4f20d767a0985eb62ce3da5c5790df35c25c1
PiyushC07 <41998079+PiyushC07@users.noreply.github.com>
2021-06-15 23:30:55 +0530

    15th June Tue 2021 - Basics(Selectors,Creating component manually & by command,css,bootstrap,Property Binding,String Interpolation

--- my-first-angular-app/src/app/server/server.component.ts
+++ my-first-angular-app/src/app/server/server.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  allowServer = false;
+  serverId = 10;
+  serverStatus = 'offline';
+  constructor() {
+    setTimeout(() => {
+      this.allowServer = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
 }
